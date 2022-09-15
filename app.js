bb6f74426f7ff27ee4e8c2ed9a4f500b238cf4f8
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-09-15 11:11:05 -0700

    Arreglar trimmer y agregar B-A;

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 function trimmer(element) {
-  if (element[0] === " ") {
-    element = element.split("");
-    element.shift();
-    return element.join("");
-  }
-  return element;
+  element = element.split(" ");
+  element = element.filter((value) => value);
+  return element.join(" ");
 }
 
 function interseccion() {
-  let a = document.querySelector(".interseccion--a").value;
-  let b = document.querySelector(".interseccion--b").value;
+  let a = document.querySelector(".interseccion__a").value;
+  let b = document.querySelector(".interseccion__b").value;
   a = a.split(",");
   b = b.split(",");
   a = a.map((element) => trimmer(element));
   b = b.map((element) => trimmer(element));
   let c = a.filter((value) => b.includes(value));
   c = c.join(", ");
-  document.querySelector(".interseccion--resultado").value = c;
+  document.querySelector(".interseccion__resultado").value = c;
 }
 
 function union() {
-  let a = document.querySelector(".union--a").value;
-  let b = document.querySelector(".union--b").value;
+  let a = document.querySelector(".union__a").value;
+  let b = document.querySelector(".union__b").value;
   a = a.split(",");
   b = b.split(",");
   a = a.map((element) => trimmer(element));
@@ -33,31 +30,34 @@ function union() {
     }
   });
   c = c.join(", ");
-  document.querySelector(".union--resultado").value = c;
+  document.querySelector(".union__resultado").value = c;
 }
 
 function diferencia() {
-  let a = document.querySelector(".diferencia--a").value;
-  let b = document.querySelector(".diferencia--b").value;
+  let a = document.querySelector(".diferencia__a").value;
+  let b = document.querySelector(".diferencia__b").value;
   a = a.split(",");
   b = b.split(",");
   a = a.map((element) => trimmer(element));
   b = b.map((element) => trimmer(element));
   let c = a.filter((value) => !b.includes(value));
   c = c.join(", ");
-  document.querySelector(".diferencia--resultado").value = c;
+  let d = b.filter((value) => !a.includes(value));
+  d = d.join(", ");
+  document.querySelector(".diferencia__resultado--a").value = c;
+  document.querySelector(".diferencia__resultado--b").value = d;
 }
 
 function complemento() {
-  let a = document.querySelector(".complemento--a").value;
-  let b = document.querySelector(".complemento--b").value;
+  let a = document.querySelector(".complemento__a").value;
+  let b = document.querySelector(".complemento__b").value;
   a = a.split(",");
   b = b.split(",");
   a = a.map((element) => trimmer(element));
   b = b.map((element) => trimmer(element));
   let c = b.filter((value) => !a.includes(value));
   c = c.join(", ");
-  document.querySelector(".complemento--resultado").value = c;
+  document.querySelector(".complemento__resultado").value = c;
 }
 
 function reiniciar(seccion) {
@@ -69,21 +69,21 @@ function reiniciar(seccion) {
 function ejemplo(num, seccion) {
   switch (num) {
     case 1:
-      document.querySelector(seccion + "--a").value = "a, b, c, d";
-      document.querySelector(seccion + "--b").value = "c, d, e, f";
+      document.querySelector(seccion + "__a").value = "a, b, c, d";
+      document.querySelector(seccion + "__b").value = "c, d, e, f";
       break;
     case 2:
-      document.querySelector(seccion + "--a").value = "1, 2, 3, 4, 5";
-      document.querySelector(seccion + "--b").value = "4, 5, 6, 7, 8";
+      document.querySelector(seccion + "__a").value = "1, 2, 3, 4, 5";
+      document.querySelector(seccion + "__b").value = "4, 5, 6, 7, 8";
       break;
     case 3:
-      document.querySelector(seccion + "--a").value = "a, e, i, o, u";
-      document.querySelector(seccion + "--b").value =
+      document.querySelector(seccion + "__a").value = "a, e, i, o, u";
+      document.querySelector(seccion + "__b").value =
         "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
       break;
     case 4:
-      document.querySelector(seccion + "--a").value = "0, 2, 4, 6, 8";
-      document.querySelector(seccion + "--b").value =
+      document.querySelector(seccion + "__a").value = "0, 2, 4, 6, 8";
+      document.querySelector(seccion + "__b").value =
         "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
     default:
       break;
